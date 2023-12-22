aaa0f9ee69f31dda72ded9abac6a73c88ff19907
NavyStack <137406386+Navystack@users.noreply.github.com>
2023-12-22 19:44:19 +0900

    Translation: init

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM perl:5-slim-bookworm as Final
+
+WORKDIR /Proxmox-KOREAN
+ARG USER=Proxmox-KOREAN
+
+RUN set -eux; \
+    apt-get update -y \
+    && DEBIAN_FRONTEND=noninteractive \
+        apt-get install -y --no-install-recommends \
+            build-essential \
+            debhelper-compat \
+            dpkg-dev \
+            git \
+            liblocale-po-perl \
+            libjson-perl \
+            lintian \
+            rsync \
+            fakeroot \
+    && git clone --depth 1 git://git.proxmox.com/git/proxmox-i18n.git . \
+    && rm -rf ./kr.po \
+    && mkdir -p /output \
+    && rm -rf /var/lib/apt/lists/* \
+    && apt-get clean \
+    && useradd --no-create-home --shell /bin/bash $USER \
+    && chown -R $USER:$USER /Proxmox-KOREAN
+COPY script/build.sh /Proxmox-KOREAN
+USER $USER
+CMD ["/Proxmox-KOREAN/build.sh"]
---
+++ script/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Converting kr.po to pve-lang-ko.js..."
+./po2js.pl -t pve kr.po >pve-lang-ko.js \
+    && echo "Conversion to pve-lang-ko.js completed." \
+    || { echo "Error converting to pve-lang-ko.js"; exit 1; }
+
+echo "Converting kr.po to pmg-lang-ko.js..."
+./po2js.pl -t pmg kr.po >pmg-lang-ko.js \
+    && echo "Conversion to pmg-lang-ko.js completed." \
+    || { echo "Error converting to pmg-lang-ko.js"; exit 1; }
+
+echo "Converting kr.po to pbs-lang-ko.js..."
+./po2js.pl -t pbs kr.po >pbs-lang-ko.js \
+    && echo "Conversion to pbs-lang-ko.js completed." \
+    || { echo "Error converting to pbs-lang-ko.js"; exit 1; }
+
+echo "Start build deb packages..."
+make deb \
+    && echo "build completed." \
+    || { echo "Error build to deb packages"; exit 1; }
+
+echo "Moving files to /output directory..."
+mv ./*-*-ko.js /output \
+    && echo "pbs-lang-ko.js moved to /output." \
+    || { echo "Error moving pbs-lang-ko.js to /output"; exit 1; }
+
+mv ./*.deb /output \
+    && echo "All deb package moved to /output." \
+    || { echo "Error moving deb packages to /output"; exit 1; }
+
+echo "Changing ownership of files..."
+
+chown -R Proxmox-KOREAN:Proxmox-KOREAN /output/*-*-ko.js \
+    && echo "Ownership changed for *-*-ko.js." \
+    || { echo "Error changing ownership for *-*-ko.js"; exit 1; }
+
+chown -R Proxmox-KOREAN:Proxmox-KOREAN /output/*.deb \
+    && echo "Ownership changed for deb packages." \
+    || { echo "Error changing ownership for deb packages.js"; exit 1; }
+
+echo "All steps completed successfully."
+exit 0
